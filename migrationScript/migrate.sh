eca5a87f9cfc759830024f611eed99533d472726
Florent <florent@apache.org>
2013-08-23 09:15:47 +0200

    monitoring module

--- migrationScript/migrate.sh
+++ migrationScript/migrate.sh
@@ -4,7 +4,7 @@
 # Don't forget the trailing / in *_MODULE_FOLDER variables
 OLD_MODULE_FOLDER="../src/modules-core/"
 NEW_MODULE_FOLDER="../modules-core/"
-MODULE_NAME="ldap" #no trailing slash in module_name
+MODULE_NAME="monitoring" #no trailing slash in module_name
 
 # end variables to set
 OLD=$OLD_MODULE_FOLDER$MODULE_NAME
